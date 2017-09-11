2169144c2d38da8a6be1adcaf58ff1085bc05cda
Carlos Lopez <liche60@users.noreply.github.com>
2017-09-11 18:31:42 -0500

    python project

--- __main__.py
+++ __main__.py
@@ -4,6 +4,10 @@
 from __future__ import print_function
 from os.path import expanduser, join, abspath
 
+import time
+import json
+import logging
+
 from pyspark.context import SparkContext
 from pyspark.sql import HiveContext
 
@@ -14,6 +18,14 @@ from pyspark.sql import HiveContext
 
 
 if __name__ == "__main__":
+    logging.getLogger("py4j").setLevel(logging.ERROR)
+
+    with open("config.json") as f_in:
+        data = json.load(f_in)
+        
+    table = data.get("table")
+    conf = SparkConf().setAppName("sleep_mapper")
+    sc = SparkContext(conf=conf)
     sqlContext = HiveContext(sc)
     sqlContext.sql("use pruebas_cuadre")
-    sqlContext.sql("FROM p17 SELECT count(*)").show()
+    sqlContext.sql("FROM "+table+" SELECT count(*)").show()
